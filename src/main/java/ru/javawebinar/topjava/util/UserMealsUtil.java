654a1026c2aeaa4a57fdec3494db2866604e4a63
теса <annilinum@mail.ru>
2022-10-09 13:02:05 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,8 +6,10 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -27,13 +29,42 @@ public class UserMealsUtil {
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
-    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<UserMealWithExcess>  filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+
+        int calories = 0;
+        for (UserMeal meal : meals) {
+            calories = calories + meal.getCalories();
+        }
+        boolean excess = calories > caloriesPerDay;
+
+        List<UserMealWithExcess> result = new ArrayList<>();
+        for (UserMeal meal : meals) {
+            if (isCorrect(meal,startTime, endTime)) {
+                result.add(toUserWithExcess(meal, excess));
+            }
+
+        }
+        return  result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO Implement by streams
-        return null;
+        int calories = meals.stream().mapToInt(m -> m.getCalories()).sum();
+        boolean excess = calories > caloriesPerDay;
+
+        return meals.stream().filter(m -> isCorrect(m, startTime, endTime) ).map(m -> toUserWithExcess (m, excess)).collect(Collectors.toList());
+    }
+
+    private static boolean isCorrect(UserMeal m, LocalTime startTime, LocalTime endTime) {
+        LocalTime mealTime = m.getDateTime().toLocalTime();
+        if (mealTime.isAfter(startTime) && mealTime.isBefore(endTime)){
+            return true;
+        }
+        return  false;
+    }
+
+    private static UserMealWithExcess toUserWithExcess (UserMeal userMeal, boolean excess){
+        return  new UserMealWithExcess(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), excess);
     }
 }
